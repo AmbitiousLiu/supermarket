62270f45053e97756d056cc915ab8c25e8b6e303
ZblSerendipity <404061768@qq.com>
2020-04-29 12:23:42 +0800

    modify request ways (request -> post)

--- src/main/java/com/example/supermarket/zbl/web/PersonInitController.java
+++ src/main/java/com/example/supermarket/zbl/web/PersonInitController.java
@@ -4,10 +4,7 @@ import com.example.supermarket.zbl.domain.Person;
 import com.example.supermarket.zbl.service.PersonInitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletResponse;
@@ -18,14 +15,14 @@ import java.io.IOException;
 public class PersonInitController {
     @Autowired PersonInitService personInitService;
 
-    @GetMapping(value = "/getInfo") void initPerson(HttpServletResponse response, @RequestParam(value = "useId" ,required = true)String useId)throws IOException {
+    @PostMapping(value = "/getInfo") void initPerson(HttpServletResponse response, @RequestParam(value = "useId" ,required = true)String useId)throws IOException {
         String content = personInitService.getAllInfo(useId);
         response.setContentType("text/json;charset=utf-8");
         response.getWriter().write(content==null ?"":content);
 
     }
 
-    @GetMapping(value = "/modifyInfo") void modifyPerson(HttpServletResponse response, @RequestParam(value = "person" ,required = true) Person person)throws IOException{
+    @PostMapping (value = "/modifyInfo") void modifyPerson(HttpServletResponse response, @RequestParam(value = "person" ,required = true) Person person)throws IOException{
         Integer integer = personInitService.modifyAllInfo(person);
         response.setContentType("text/json;charset=utf-8");
         response.getWriter().write(integer==0 ?"修改成功！":"修改失败！");
