e44777c4c5618efc4cae880dc80c70d040727922
ZblSerendipity <404061768@qq.com>
2020-05-03 16:06:06 +0800

    optimize pages and add design used Vue

--- src/main/resources/static/zbl/js/supermarket_pcenter.js
+++ src/main/resources/static/zbl/js/supermarket_pcenter.js
@@ -1,27 +1,102 @@
 $(function(){
-
-
-
-	$.post(
-	"http://localhost:8080/Info/getInfo",
-	{},
-	function(data){
-
-		var json = eval(data);
-
-		$("#name_pos").text(json[0].name+"/"+json[0].position)
-		$("#gender").val(json[0].gender);
-		$("#position").val(json[0].position);
-		$("#age").val(json[0].age);
-		$("#stu_num").val(json[0].stu_num);
-		$("#seniority").val(json[0].seniority);
-		$("#region").val(json[0].region);
-		$("#salary").val(json[0].salary);
-	},
-		"json"
 	
-)
-	
+	var v1 = new Vue({
+		el:"#center_of_right",
+		data:{
+
+			vname:"",
+			mname:"",
+
+			vgender:"",
+			mgender:"",
+
+			vposition:"",
+			mposition:"",
+
+			vage:"",
+			mage:"",
+
+			vstu_num:"",
+			mstu_num:"",
+
+			vregion:"",
+			mregion:"",
+
+			vsalary:"",
+
+			vseniority:"",
+			mseniority:""
+			
+		},
+		mounted(){
+			this.getInfo();
+
+		},
+		methods:{
+			getInfo(){
+				ajax({
+					method:HTTP_METHOD.POST,
+					url:"http://localhost:8080/Info/getInfo",
+					async:true,
+					type:HTTP_RESPONSE_TYPE.JSON,
+					callback:(data)=>{
+
+						var json = eval(data);
+
+						this.vname = json[0].name+"/"+json[0].position;
+						this.mname = json[0].name;
+
+						this.vgender =  json[0].gender;
+						this.mgender =  json[0].gender;
+
+						this.vposition = json[0].position;
+						this.mposition = json[0].position;
+
+						this.vage    = json[0].age;
+						this.mage    = json[0].age;
+
+						this.vstu_num= json[0].stu_num;
+						this.mstu_num= json[0].stu_num;
+
+						this.vseniority = json[0].seniority;
+						this.mseniority = json[0].seniority;
+
+						this.vregion = json[0].region;
+						this.mregion = json[0].region;
+
+						this.vsalary = json[0].salary;
+					}
+				})
+
+
+			}
+		}
+
+			
+
+	})
+//	
+//	$.post(
+//	"http://localhost:8080/Info/getInfo",
+//	{},
+//	function(data){
+//
+//		var json = eval(data);
+//
+//		$("#name_pos").text(json[0].name+"/"+json[0].position)
+//		$("#gender").val(json[0].gender);
+//		$("#position").val(json[0].position);
+//		$("#age").val(json[0].age);
+//		$("#stu_num").val(json[0].stu_num);
+//		$("#seniority").val(json[0].seniority);
+//		$("#region").val(json[0].region);
+//		$("#salary").val(json[0].salary);
+//	},
+//		"json"
+//	
+//)
+
+
 	$("#modify").click(function(){
 		
 		var name = $("#modi_name").val();
@@ -31,7 +106,6 @@ $(function(){
 		var region = $("#modi_region").val();
 		var position = $("#modi_pos").val();
 		var stu_num = $("#modi_num").val();
-
 
 
 
