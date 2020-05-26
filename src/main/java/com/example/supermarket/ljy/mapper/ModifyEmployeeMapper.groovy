1752f801cb22d64cde6d47a48df00b25b746e88a
Johnny Leo <997510177@qq.com>
2020-05-26 21:22:51 +0800

    add layui

--- src/main/java/com/example/supermarket/ljy/mapper/ModifyEmployeeMapper.groovy
+++ src/main/java/com/example/supermarket/ljy/mapper/ModifyEmployeeMapper.groovy
@@ -2,6 +2,7 @@ package com.example.supermarket.ljy.mapper
 
 import org.apache.ibatis.annotations.Delete
 import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Param
 import org.apache.ibatis.annotations.Update
 
 /**
@@ -12,9 +13,9 @@ import org.apache.ibatis.annotations.Update
 interface ModifyEmployeeMapper {
 
     @Delete("delete from employee where #{tiaojian} = #{tiaojianzhi}")
-    Integer delete(String tiaojian, String tiaojianzhi)
+    Integer delete(@Param("tiaojian")String tiaojian,@Param("tiaojianzhi") String tiaojianzhi)
 
     @Update("update employee set #{gaixinxi} = #{xiugaizhi} where #{tiaojian} = #{tiaojianzhi}")
-    Integer update(String tiaojian, String tiaojianzhi, String gaixinxi, String xiugaizhi)
+    Integer update( @Param("tiaojian")String tiaojian, @Param("tiaojianzhi") String tiaojianzhi, @Param("gaixinxi") String gaixinxi,@Param("xiugaizhi") String xiugaizhi)
 
 }
